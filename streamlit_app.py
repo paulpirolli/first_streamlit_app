0abe9f82eb8858e5c43d80a672015f67449fb636
Paul Pirolli <paulpirolli@gmail.com>
2022-09-01 15:27:32 -0400

    updated list index

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pnd
 
 my_fruit_list = pnd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 st.title('My Parents\' New Healthy Diner')
 
